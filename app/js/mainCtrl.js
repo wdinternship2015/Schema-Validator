bd56a2ded3194340a2a2c43be88ec87b8ab42e32
elisa.yan <elisa.yan@workday.com>
2015-07-08 10:20:00 -0700

    fronted changed
    added id’s to element for testing
    moved save as file name generator to mainCtrl.js

--- app/js/mainCtrl.js
+++ app/js/mainCtrl.js
@@ -19,7 +19,6 @@ angular.module('evaluator').controller('uploadController',["$scope", "$http", fu
   $scope.submit = function() {
     //if no input or schema file, do not send to server and clear text area 
     if($scope.inputFile == null || $scope.schemaFile == null){
-      alert("clear");
       $scope.textArea="";
       return;
     }
@@ -29,44 +28,60 @@ angular.module('evaluator').controller('uploadController',["$scope", "$http", fu
     formData.append("inputFile", $scope.inputFile);
     formData.append("direction", $scope.direction);
     $http({
-       url: "http://localhost:8080/ValidateService/webapi/runSchema/byFileName",
+       url: "http://localhost:8080/ValidateService/webapi/runSchema",
+//       url: "http://localhost:8080/ValidateService/webapi/testResponse/POSTxml",
        data: formData,
        method: 'POST',
        transformResponse: function(data){
-         return data;
+    	   return data;
        },
        headers : {'Content-Type' : undefined},
      }).success(function (data, status, headers, config) {
-        var extIndex = data.indexOf(".");
-        var space = data.indexOf(" ", extIndex);
-        var fileName = data.substring(0, space);
-        var outText = data.substring(space + 1);
-        //alert(fileName + "\n" + outText);
         //display the text response with the correct style   
         //enable button so user can save response in file and choose a file name 
-        $scope.textArea = outText;
+        $scope.textArea = data;
         var text = document.querySelector('#comment');
-        text.value = outText;
+        text.value = data;
         text.style.color="black";
         $scope.disable = false;
-        $scope.inputName = "fileName";
-        (document.querySelector('#enter_name')).value=fileName;  
+        var saveAsName = getSaveAsResultName($scope.inputFileName, $scope.direction);
+        $scope.inputName = saveAsName;
      }).error(function (data, status, headers, config) {
-        var extIndex = data.indexOf(".");
-        var space = data.indexOf(" ", extIndex);
-        var fileName = data.substring(0, space);
-        var outText = data.substring(space + 1);
-
-        //display the text error response with the correct style
+       //display the text error response with the correct style
         //disable button so user cannot save error response
-        $scope.textArea = outText;
+        $scope.textArea = data;
         var text = document.querySelector('#comment');
-        text.value = outText;
+        text.value = data;
         text.style.color="red";
         $scope.disable = false;
-        $scope.inputName =fileName;
-        (document.querySelector('#enter_name')).value=fileName;  
+        $scope.inputName = getSaveAsErrorName($scope.inputFileName, $scope.direction);
      });
    }
 }]);
+
+
+function getSaveAsResultName(inputFileName, direction) {
+	var i = inputFileName.indexOf(".") - 1;
+	var name = inputFileName.substring(0, i);
+	if (direction.toUpperCase() === "TXT TO XML")  {		
+		return  name + "_TextToXML.xml";
+	} else if (direction.toUpperCase() === "XML TO TXT") {
+		return name + "_XMLToText.txt";
+	} else {
+		return result;
+	}
+}
+
+function getSaveAsErrorName(inputFileName, direction) {
+	var i = inputFileName.indexOf(".") - 1;
+	var name = inputFileName.substring(0, i);
+	if (direction.toUpperCase() === "TXT TO XML")  {
+		return name + "_TextToXML_error.txt";
+	} else if (direction.toUpperCase() === "XML TO TXT") {
+		return name + "_XMLToText_error.txt";
+	} else {
+		return "error.txt";
+	}
+}
+
 
