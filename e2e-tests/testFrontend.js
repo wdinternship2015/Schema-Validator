1922cbc5e8e583228a42bfccc1f02bfbce9bfc1a
elisa.yan <elisa.yan@workday.com>
2015-07-08 18:28:36 -0700

    AngularJS test folder

--- e2e-tests/testFrontend.js
+++ e2e-tests/testFrontend.js
@@ -1,11 +1,6 @@
 'use strict';
-var backEndMocks = require('./httpBackEndMocks');
-
-var ptor;
-
 
 describe('text schema validator', function() {
-
 
   it('should automatically open instruction modal window', function() {
     browser.get('index.html');
@@ -28,29 +23,28 @@ describe('text schema validator', function() {
     	var fileToUpload = 'testFiles/transamerica_expected_text.txt',
         absolutePath = path.resolve(__dirname, fileToUpload);
     	element(by.id('inputFile')).sendKeys(absolutePath);  
-    	expect(element(by.id('inputFile')).isPresent()).toBe(true);   
-    	expect(element(by.id('inputFile')).getAttribute('value')).not.toEqual('');    
+    	expect(element(by.id('inputFile')).getAttribute('value')).toContain('transamerica_expected_text.txt');    
     });
 
     it('should refuse upload if input file type is incorrect', function() {   	
     	var fileToUpload = 'testFiles/transamerica_inbound_loans.xsd',
         absolutePath = path.resolve(__dirname, fileToUpload);
     	element(by.id('inputFile')).sendKeys(absolutePath);  
-    	expect(element(by.id('inputFile')).getText()).toEqual('');    
+    	expect(element(by.id('inputFile')).getAttribute('value')).toEqual('');    
     });
     
     it('should upload if schema input file type is correct', function() {   	
     	var fileToUpload = 'testFiles/transamerica_inbound_loans.xsd',
         absolutePath = path.resolve(__dirname, fileToUpload);
     	element(by.id('schemaFile')).sendKeys(absolutePath);  
-    	expect(element(by.id('schemaFile')).isPresent()).toBe(true);    
+    	expect(element(by.id('schemaFile')).getAttribute('value')).toContain('transamerica_inbound_loans.xsd');    
     });
     
     it('should refuse upload if schema file type is incorrect', function() {   	
     	var fileToUpload = 'testFiles/transamerica_expected_text.txt',
         absolutePath = path.resolve(__dirname, fileToUpload);
     	element(by.id('schemaFile')).sendKeys(absolutePath);  
-    	expect(element(by.id('schemaFile')).getText()).toEqual('');    
+    	expect(element(by.id('schemaFile')).getAttribute('value')).toEqual('');    
     });
     
   });
@@ -62,36 +56,10 @@ describe('text schema validator', function() {
 	      element(by.id('modalOK')).click();
 	    });
 
-	    it('should always has value', function() {   	
+	    it('should always have value', function() {   	
 	    	expect(element(by.id('direction')).getAttribute('value')).not.toEqual('');    
 	    });	    
   });
-  
-  describe('submit form', function() {
-
-	    beforeEach(function() {
-	      browser.get('index.html');
-	      element(by.id('modalOK')).click();
-	    });
-
-	    it('should get file name and result message', function() {   	
-	    	ptor = protractor.getInstance();  
-	    	ptor.addMockModule('httpBackEndMocks', backEndMocks.build([backEndMocks.serverMsg]));
-	    	
-	    	var fileToUpload = 'testFiles/transamerica_expected_text.txt',
-	        absolutePath = path.resolve(__dirname, fileToUpload);
-	    	element(by.id('inputFile')).sendKeys(absolutePath); 
-	    	
-	       	var fileToUpload = 'testFiles/transamerica_inbound_loans.xsd',
-	        absolutePath = path.resolve(__dirname, fileToUpload);
-	    	element(by.id('schemaFile')).sendKeys(absolutePath);  
-	    	
-	    	element(by.id('submitForm')).click();
-	    	
-	    	expect(element(by.id('enter_name')).getAttribute('value')).toEqual('filename.ext'); 
-	    	expect(element(by.id('comment')).getAttribute('value')).toEqual('result string from server'); 
-	    });	    
-  });
-
+ 
 });
 
