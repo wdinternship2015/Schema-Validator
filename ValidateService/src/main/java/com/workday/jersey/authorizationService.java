ddba758bc0588211cdbdb61f3f7da15e17837745
elisa.yan <elisa.yan@workday.com>
2015-07-10 15:01:59 -0700

    add login verification
    pass if password and username are 2 chars or longer, denied otherwise

---
+++ ValidateService/src/main/java/com/workday/jersey/authorizationService.java
@@ -0,0 +1,72 @@
+package com.workday.jersey;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+
+ 
+/**
+ * Login authorization Service provider for http requests
+ * 
+ * Input requirement: MediaType.APPLICATION_JSON
+ * 		- field "username", type string
+ * 		- field "password", type string
+ * 
+ * 	url: /ValidateService/webapi/login
+ * 
+ * 
+ * @author Elisa Yan
+ * @author Britney Wong
+ * 
+ * @since 7.10.2015
+ */
+@Path("/login")
+public class authorizationService {
+	final int SUCCESS = 200;
+	final int CLIENT_FAIL = 400;
+	final int SERVER_ERROR = 500;
+		
+	/**
+	 * Processes input and schema processing requests
+	 * @param formParams FormDataMultipart object in the Request body
+	 * @return Response with result as a formatted string
+	 */
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)    
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response authLogin(Credential credential) {
+//	public Response authLogin(String id) {
+		System.out.println("received request");
+		System.out.println(credential.toString());
+		int statusInt;
+		String outputString;
+		ResponseBuilder response;
+	
+		
+		if (credential.getUsername().length()>2 && credential.getPassword().length() > 2) {
+			statusInt = SUCCESS;
+			outputString = "pass";
+		} else {
+			statusInt = CLIENT_FAIL;
+			outputString = "denied";
+		}
+		System.out.println("success: " + statusInt + "\noutputString: " + outputString);
+		response = Response.status(statusInt).entity(outputString);
+//		response = Response.status(SUCCESS).entity("somethign");
+		
+		//CORS HttpResponse header
+		response.header("Access-Control-Allow-Origin", "*");
+		response.header("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS, X-XSRF-TOKEN");
+		response.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
+		
+		//End of Header
+	    return response.build();
+	}
+	
+
+}
