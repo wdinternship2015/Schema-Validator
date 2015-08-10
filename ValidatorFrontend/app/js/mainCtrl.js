4a2c3bf9c9423f5fd737811252598b6343ef4c08
britney.wong <britney.wong@workday.com>
2015-08-10 15:28:37 -0700

    Login 2!

---
+++ ValidatorFrontend/app/js/mainCtrl.js
@@ -0,0 +1,22 @@
+angular.module('evaluator').controller('mainCtrl', function($scope, $localStorage, $window, authService){
+  /*$scope.$storage = $localStorage.$default({
+    token: ""
+  });*/
+  $scope.parentToken = {};
+  $scope.parentToken.token = "";
+  //$scope.parentToken.token = $scope.$storage.token; 
+  //$scope.parentToken.token = "test";
+  //if($scope.parentToken.token == "ImAToken"){
+  $scope.$watch('parentToken.token', function(){
+
+  if($scope.parentToken.token.length > 0){
+    //console.log("Length is greater than zero");
+    //alert("Navigating to MainContent");
+    $scope.pageContent = "partials/mainContent.html";
+  } else {
+    //alert('Here!');
+    //$window.location.href = 'http://localhost:8000/#/index.html';
+    $scope.pageContent = "partials/login.html";
+  }
+ });
+});
